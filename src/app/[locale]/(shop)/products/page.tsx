9e15032c28641699d40353fbb7653174ef8885d6
Yahia bawadekji <yahya2201129@gmail.com>
2026-05-11 21:23:22 +0300

    Added netlify stuff

--- src/app/[locale]/(shop)/products/page.tsx
+++ src/app/[locale]/(shop)/products/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { setRequestLocale } from "next-intl/server";
 import { ProductsView } from "@/shop/views/ProductsView";
 
@@ -8,5 +9,15 @@ type Props = Readonly<{
 export default async function ProductsPage({ params }: Props) {
   const { locale } = await params;
   setRequestLocale(locale);
-  return <ProductsView />;
+  return (
+    <Suspense
+      fallback={
+        <div className="mx-auto max-w-7xl px-4 py-8 text-sm text-secondary">
+          Loading…
+        </div>
+      }
+    >
+      <ProductsView />
+    </Suspense>
+  );
 }
